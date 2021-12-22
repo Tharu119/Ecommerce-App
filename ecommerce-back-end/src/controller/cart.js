f5e3bd1760c9c3f6bac869b34058a5f959453b51
Tharu119 <tharukamadhushanka9558@gmail.com>
2021-12-22 07:25:19 +0530

    cart controller

--- ecommerce-back-end/src/controller/cart.js
+++ ecommerce-back-end/src/controller/cart.js
@@ -1,16 +1,67 @@
 const Cart = require('../models/cart');
 
 exports.addItemToCart = (req, res) =>{
-    
-    const cart = new Cart({
-        user: req.user._id,
-        cartItems: req.body.cartItems //model eke tiyana name eka.
-    });
 
-    cart.save((error, cart)=>{
-        if (error) return res.status(400).json({ error });
+    Cart.findOne({ user: req.user._id})
+    .exec((error, cart)=>{
+        if(error) return res.status(400).json({ error});
         if(cart){
-            return res.status(201).json({ cart});
+
+            // res.status(200).json({ message: cart });
+            //cart eke ekama item eka add keruweoth error msg ekak watei.
+
+            const product =  req.body.cartItems.product;
+            const item = cart.cartItems.find(c=> c.product == product);
+
+            if(item){
+
+                Cart.findOneAndUpdate({ "user": req.user._id, "cartItems.product":product },{
+                    "$set":{
+                        "cartItems": {
+                            ...req.body.cartItems,
+                            quantity:item.quantity + req.body.cartItems.quantity
+                        }
+                    }
+                    })//meya exec karanne nathuwa run nowe.
+                    .exec((error, _cart)=>{
+                        if(error) return res.status(400).json({ error});
+                        if(_cart){
+                            return res.status(201).json({ cart});
+                        }  
+                    })
+                
+
+            }else{
+
+                Cart.findOneAndUpdate({ user: req.user._id},{
+                    "$push":{
+                        "cartItems": req.body.cartItems
+                    }
+                    })//meya exec karanne nathuwa run nowe.
+                    .exec((error, _cart)=>{
+                        if(error) return res.status(400).json({ error});
+                        if(_cart){
+                            return res.status(201).json({ cart});
+                        }  
+                    })
+            }
+
+
+        }else{
+            //mema item eka kalin add kara nomatinam aluten cart ekata add karai.
+            const cart = new Cart({
+                user: req.user._id,
+                cartItems: [req.body.cartItems] //model eke tiyana name eka.
+            });
+        
+            cart.save((error, cart)=>{
+                if (error) return res.status(400).json({ error });
+                if(cart){
+                    return res.status(201).json({ cart});
+                }
+            });
         }
     });
+    
+
 };  
